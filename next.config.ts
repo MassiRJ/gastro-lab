95522ec465c607001718d4f202bddd247c87b12b
Administrador <admin@hospital.com>
2025-12-21 16:46:53 -0500

    Fix next.config.ts type error and ignore lint via flags

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // Esto permite que el build continúe aunque haya advertencias de estilo
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
